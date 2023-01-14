c50fa6c10189989e7c0d1a0189f88feecd0743fb
ParvesSuzon <parvessuzon@gmail.com>
2023-01-14 11:37:55 +0600

    Create Lib.py

---
+++ Lib.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+import math
+import json
+import time
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import NearestNeighbors
+#from sklearn.externals import joblib
+import scipy.sparse
+from scipy.sparse import csr_matrix
+import warnings; warnings.simplefilter('ignore')
+%matplotlib inline
+
+# Input data files are available in the "../input/" directory.
+# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
+
+import os
+for dirname, _, filenames in os.walk('/kaggle/input'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
